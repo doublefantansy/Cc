85cafe2a9dd957e43434daa3c7367c00ac0edf0a
cc <doublefantansy@outlook.com>
2019-06-10 09:43:54 +0800

    日常

--- app/src/main/java/hzkj/cc/test/MainActivity.java
+++ app/src/main/java/hzkj/cc/test/MainActivity.java
@@ -49,9 +49,9 @@ public class MainActivity extends AppCompatActivity {
                         .show();
             }
         });
-        loadLayout.setLoadMoreListenner(new CcRrefreshAndLoadMoreRecyclerView.LoadMoreListenner() {
+        loadLayout.setRefreshListenner(new CcRrefreshAndLoadMoreRecyclerView.RefreshListenner() {
             @Override
-            public void loadMore() {
+            public void refresh() {
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
