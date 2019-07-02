ea2224c779727c92db0792210450f18d31ef517b
cc <doublefantansy@outlook.com>
2019-07-02 15:27:18 +0800

    日常

--- ccrecyclerview/src/main/java/hzkj/cc/ccrecyclerview/CcRrefreshAndLoadMoreRecyclerView.java
+++ ccrecyclerview/src/main/java/hzkj/cc/ccrecyclerview/CcRrefreshAndLoadMoreRecyclerView.java
@@ -215,7 +215,7 @@ public class CcRrefreshAndLoadMoreRecyclerView extends RecyclerView {
                 } else {
                     isCanR = false;
                 }
-                if ((lastVisibleItem == insideAdapter.getItemCount() && (lastCompleteVisibleItem == insideAdapter.getItemCount() + 1 | lastCompleteVisibleItem == insideAdapter.getItemCount())) & adapter.footholder != null)
+                if ((lastCompleteVisibleItem == insideAdapter.getItemCount() + 1 || ((lastCompleteVisibleItem == insideAdapter.getItemCount()) && lastVisibleItem == insideAdapter.getItemCount()) & adapter.footholder != null))
                 {
                     if (!isLoading & !isRefresh & loadMoreEnable) {
                         Log.d("cctag", "in1");
